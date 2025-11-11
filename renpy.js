e4d99f5d9a83c29d41646389b8c5572ea1c54d7f
PavelKosik <pavel.k@webgroup-limited.com>
2025-11-11 20:31:37 +0100

    Update renpy.js

--- renpy.js
+++ renpy.js
@@ -106,7 +106,7 @@ Module["expectedDataFileDownloads"]++;
           var packageData = xhr.response;
           callback(packageData);
         } else {
-          throw new Error("TEST ERROR: " + xhr.response);
+          throw new Error("TEST ERROR: " + xhr.toString());
         }
       };
       xhr.send(null);
